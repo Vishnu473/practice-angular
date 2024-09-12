491b4e69292ab65c629a61a9320b10bd5a3d3386
Vishnu Kishore <tarinivishnukishore123@gmail.com>
2024-09-12 11:40:55 +0000

    ngOnInit and Constructor In angular LifeCycleHooks.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,8 +7,20 @@ import { Component, Input, OnChanges } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnChanges, OnInit {
   @Input() pUserName: string = '';
+
+  constructor() {
+    console.log(
+      "This is called when component is created. Keep it simple. Don't access DOM."
+    );
+  }
+
+  ngOnInit() {
+    console.log(
+      'This is called only once after component is Initiated in the entire component LifeCycle. Used for API calls.'
+    );
+  }
 
   ngOnChanges() {
     console.log('Some Change was detected');
